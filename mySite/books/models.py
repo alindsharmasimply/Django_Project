10fac7345bb4cae9bcd6251464626e98f586b61f
Alind Sharma <alindsharmasimply@gmail.com>
2017-10-14 23:57:03 +0530

    Useless Changes

--- mySite/books/models.py
+++ mySite/books/models.py
@@ -5,12 +5,29 @@ from django.db import models
 
 # Create your models here.
 
+
 class Book(models.Model):
-	def __str__(self):
-		return self.name + ' - ' + self.author
+    def __str__(self):
+        return self.name + ' - ' + self.author
+
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
 
 
-	name = models.CharField(max_length = 100)
-	author = models.CharField(max_length = 100)
-	price = models.CharField(max_length = 100)
-	category = models.CharField(max_length = 100)
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
